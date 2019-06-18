57fd5ad86faa37a88902e7913ea9d20703366d45
Waga <wagaodongo@gmail.com>
2019-06-18 22:02:46 +0300

    start

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import EmojiDict from './components/EmojiDict';
+
+export default class App extends Component {
+	render() {
+		return <EmojiDict />;
+	}
+}
